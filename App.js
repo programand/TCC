ae529b8103d4f5c64477014d7cce7403edf970d9
programand <yara.yyg@gmail.com>
2021-06-16 02:26:43 +0000

    começo dos serviços

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
             <Stack.Screen name="Entra" component={Entra}/> */}
          </Stack.Navigator>  
               
-    //   </NavigationContainer>
+       </NavigationContainer>
   
     
   );
